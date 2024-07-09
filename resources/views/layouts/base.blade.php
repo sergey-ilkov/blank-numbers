8f404927488d7b6d9b930455986958db93d08d48
Sergey <ilkov1103@gmail.com>
2024-07-09 08:48:30 +0300

    @v1.2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,9 +11,13 @@
 
     <link rel="stylesheet" href="{{ asset('css/style.css') . '?v=' . rand(10, 1000) }}">
 
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
+        integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
+        integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
 
     <script src="{{ asset('js/script.js') . '?v=' . rand(10, 1000)  }}" defer></script>
 
