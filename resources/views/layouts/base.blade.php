3909ec260f49b10211e623c4adb73e03bdb09ff9
Sergey <ilkov1103@gmail.com>
2024-07-09 09:20:03 +0300

    @v1.2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,11 +13,11 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
         integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous"
-        referrerpolicy="no-referrer"></script>
+        referrerpolicy="no-referrer" defer></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
         integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous"
-        referrerpolicy="no-referrer"></script>
+        referrerpolicy="no-referrer" defer></script>
 
     <script src="{{ asset('js/script.js') . '?v=' . rand(10, 1000)  }}" defer></script>
 
